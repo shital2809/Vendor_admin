e715d22be4e0b4a4f272afe9c93e8e6978c2545d
shital2809 <shitalkendre049@gmail.com>
2025-07-06 22:43:58 +0530

    initial commit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false // Set to true in production with valid SSL certificates
+    },
 });
 
 pool.connect()
